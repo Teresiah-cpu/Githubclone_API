ee6667de1caece60b466ad0b79938ac7e2a39709
Teresiah-cpu <terrywambo2018@gmail.com>
2022-04-18 15:29:34 +0300

    add httpclient

--- src/app/profile.service.ts
+++ src/app/profile.service.ts
@@ -1,10 +1,22 @@
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
+import { HttpClient } from  '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProfileService {
+   private user: any;
+  private username: string;
+  private _Url = 'https://api.github.com/users';
 
-  constructor() { }
+  constructor(private http:HttpClient ) { this.username="Teresiah-cpu";}
+   ngOnInit() {
+    // GET request with response type <any>
+    this.http.get<any>('https://api.github.com/users').subscribe((data) => {
+      this.username = data.total;
+      console.log(this.username);
+    });
+
+  
 }
