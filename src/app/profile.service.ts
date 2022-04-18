f3607d22787d577db48fb9207c3708116d32d635
Teresiah-cpu <terrywambo2018@gmail.com>
2022-04-18 15:56:44 +0300

    add get functions

--- src/app/profile.service.ts
+++ src/app/profile.service.ts
@@ -12,7 +12,7 @@ export class ProfileService {
 
   constructor(private http:HttpClient ) { this.username="Teresiah-cpu";}
    ngOnInit() {
-    // GET request with response type <any>
+    //getting requests
     this.http.get<any>('https://api.github.com/users').subscribe((data) => {
       this.username = data.total;
       console.log(this.username);
@@ -20,3 +20,23 @@ export class ProfileService {
 
   
 }
+//getting profile info
+  getUserInfo() {
+    return this.http.get('https://api.github.com/users/' + this.username); 
+  }
+    //get repo information
+  getUserRepos() {
+    return this.http.get(
+      'https://api.github.com/users/' + this.username + '/repos'
+    );  }
+      getProfiles() {
+    return this.http.get<any[]>(this._Url);  }
+     getProfileInfo(){  
+    return this.http.get('https://api.github.com/users/'+this.username+'?acess_token='+environment.apiKey);    }   
+  
+  getRepos(user: any){
+    let userrepo = 
+    this.http.get('https://api.github.com/users/'+this.username+'/repos?acess_token='+environment.apiKey); 
+    console.log(userrepo)
+    return userrepo    
+  }
